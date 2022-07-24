41d92b9bd64b2a674004909c2a76620ba75e4b1f
Amr Mekki <amr.mekki21@gmail.com>
2022-07-24 14:24:05 +0200

    33

--- 03 Understanding the Basics/app.js
+++ 03 Understanding the Basics/app.js
@@ -1,12 +1,31 @@
 const http = require("http");
+const fs = require('fs');
+const { parse } = require("path");
+const { measureMemory } = require("vm");
 
 const server = http.createServer((req, res) => {
     const url = req.url;
+    const method = req.method; 
     if(url === '/'){
         res.write('<html>');
         res.write('<head><title>My First Page</title></head>');
-        res.write('<body><form action="/message" method = "POST"><input type = "text"><button type="submit">Send</button></form></body>');
+        res.write('<body><form action="/message" method = "POST"><input type = "text" name="message"><button type="submit">Send</button></form></body>');
         res.write('</html>');
+        return res.end();
+    }
+    if(url === '/message' && method === 'POST'){
+        const body = [];
+        req.on('data', (chunk)=>{
+            console.log(chunk);
+            body.push(chunk);
+        });
+        req.on('end', () =>{
+            const parsedBody = Buffer.concat(body).toString();
+            const message = parsedBody.split('=')[1];
+            fs.writeFileSync('message.txt', message);
+        })
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
         return res.end();
     }
     // process.exit();

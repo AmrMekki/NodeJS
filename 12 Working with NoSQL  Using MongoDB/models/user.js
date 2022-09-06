5b248afcc27b798005f3cf0d593f3195c6106286
Amr Mekki <amr.mekki21@gmail.com>
2022-09-07 01:52:42 +0200

    195

--- 12 Working with NoSQL  Using MongoDB/models/user.js
+++ 12 Working with NoSQL  Using MongoDB/models/user.js
@@ -25,10 +25,13 @@ class User {
       newQuantity = this.cart.items[cartProductIndex].quantity + 1;
       updatedCartItems[cartProductIndex].quantity = newQuantity;
     } else {
-      updatedCartItems.push({ productId: new ObjectId(product._id), quantity: 1 });
+      updatedCartItems.push({
+        productId: new ObjectId(product._id),
+        quantity: 1,
+      });
     }
     const updatedCart = {
-      items: updatedCartItems
+      items: updatedCartItems,
     };
     const db = getDb();
     return db
@@ -37,6 +40,26 @@ class User {
         { _id: new ObjectId(this._id) },
         { $set: { cart: updatedCart } }
       );
+  }
+  getCart() {
+    const db = getDb();
+    const productIds = this.cart.items.map((i) => {
+      return i.productId;
+    });
+    return db
+      .collection("products")
+      .find({ _id: { $in: productIds } })
+      .toArray()
+      .then((products) => {
+        return products.map((p) => {
+          return {
+            ...p,
+            quantity: this.cart.items.find((i) => {
+              return i.productId.toString() === p._id.toString();
+            }).quantity
+          };
+        });
+      });
   }
   static findById(userId) {
     const db = getDb();
